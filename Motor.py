f9b4b6ab45b7934bb85664b0e4d4ef37da397aed
Thomas Lauer <thomaslauer@users.noreply.github.com>
2018-01-03 14:19:01 -0600

    Moved Encoder to a module named Motor to hold all the motor related code. Added main.py as main method

--- Motor.py
+++ Motor.py
@@ -1,7 +1,4 @@
 import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)
 
 class Encoder:
     positionKey = [[0,0], [1,0], [1,1], [0,1]]
@@ -10,41 +7,37 @@ class Encoder:
         self.chanA = chanA
         self.chanB = chanB
 
+        # Set two channels as inputs
         GPIO.setup(self.chanA, GPIO.IN)
         GPIO.setup(self.chanB, GPIO.IN)
 
+        # Register callback with both channels
         GPIO.add_event_detect(self.chanA, GPIO.BOTH, callback=self.gpio_callback)
         GPIO.add_event_detect(self.chanB, GPIO.BOTH, callback=self.gpio_callback)
 
-        self.currentPosition = self.findPosition(GPIO.input(self.chanA), GPIO.input(self.chanB))
+        # Set the current stage to the current position of the motor
+        self.currentStage = self.findPosition(GPIO.input(self.chanA), GPIO.input(self.chanB))
+
+        # Default to a position of 0
         self.pos = 0
     
     def gpio_callback(self, gpio_id):
         channelA = GPIO.input(self.chanA)
         channelB = GPIO.input(self.chanB)
 
-        self.lastPosition = self.currentPosition
-        self.currentPosition = self.findPosition(channelA, channelB)
+        self.lastPosition = self.currentStage
+        self.currentStage = self.findPosition(channelA, channelB)
 
-        if((self.lastPosition + 1) % 4 == self.currentPosition):
+        if((self.lastPosition + 1) % 4 == self.currentStage):
             self.pos = self.pos + 1
         else:
             self.pos = self.pos - 1
     
     def findPosition(self, channelA, channelB):
         currentList = [channelA, channelB]
-        currentPosition = -1
+        stage = -1
         for i in range(4):
             if self.positionKey[i] == currentList:
-                currentPosition = i
+                stage = i
 
-        return currentPosition
-
-
-GPIO.setup(17, GPIO.IN)
-GPIO.setup(18, GPIO.IN)
-
-a = Encoder(17, 18)
-
-while True:
-    time.sleep(0.01)
+        return stage
