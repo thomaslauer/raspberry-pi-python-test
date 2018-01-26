5d050671a11f20c2d2225af46936efc9c1449f3a
Thomas Lauer <thomaslauer@users.noreply.github.com>
2018-01-25 22:10:01 -0800

    Started work on robot simulation software

---
+++ sim/robotsim.py
@@ -0,0 +1,36 @@
+import math
+import matplotlib.pyplot as plt
+
+g = 9.8
+t = 0.01
+
+class Robot:
+
+    def __init__(self, cogLength, mass):
+        # need to add starting positions (velocity, angle)
+        # maybe make a way to do list of commands to execute at
+        # a certain time, etc
+        self.cogLength = cogLength
+        self.mass = mass
+        self.angle = math.pi/2
+        self.angularVelocity = 0
+        self.angularAcceleration = 0
+    
+    def updatePhysics(self):
+        self.angularAcceleration = (g/self.cogLength) * math.sin(self.angle)
+        self.angularVelocity += self.angularAcceleration * t
+        self.angle += self.angularVelocity * t
+    
+
+def main():
+    robot = Robot(0.5, 1)
+
+    # define the time in seconds of the simulation
+    time = 5
+
+    for i in range(int(time / t)):
+        robot.updatePhysics()
+        print("Angle: {:2.4f}  Velocity: {:2.4f}  Acceleration: {:2.4f}"
+            .format(robot.angle * 180 / math.pi, robot.angularVelocity, robot.angularAcceleration))
+
+main()
